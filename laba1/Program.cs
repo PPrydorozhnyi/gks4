7ad1253d5612e8187e34e74518a0b9428e396683
Ppridorozhny <Peter35128@gmail.com>
2019-03-24 13:05:25 +0200

    simplex method

--- laba1/Program.cs
+++ laba1/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using laba1.simplex;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -14,9 +15,34 @@ namespace laba1
         [STAThread]
         static void Main()
         {
-            Application.EnableVisualStyles();
+            /*Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new Form1());*/
+
+            double[,] table = { {25, -3,  5},
+                                {30, -2,  5},
+                                {10,  1,  0},
+                                { 6,  3, -8},
+                                { 0, -6, -5} };
+
+            double[] result = new double[2];
+            double[,] table_result;
+            Simplex S = new Simplex(table);
+            table_result = S.Calculate(result);
+
+            Console.WriteLine("Решенная симплекс-таблица:");
+            for (int i = 0; i < table_result.GetLength(0); i++)
+            {
+                for (int j = 0; j < table_result.GetLength(1); j++)
+                    Console.Write(table_result[i, j] + " ");
+                Console.WriteLine();
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("Решение:");
+            Console.WriteLine("X[1] = " + result[0]);
+            Console.WriteLine("X[2] = " + result[1]);
+            Console.ReadLine();
         }
     }
 }
