b30cd37939065cb8e00accc885f7311f2151cc7a
TastyPupsik <vadim.gerashchenko@gmai.com>
2019-03-24 12:47:56 +0200

    git test

--- laba1/Form1.cs
+++ laba1/Form1.cs
@@ -10,9 +10,9 @@ using System.Windows.Forms;
 
 namespace laba1
 {
-    public partial class laba1 : Form
+    public partial class Form1 : Form
     {
-        public laba1()
+        public Form1()
         {
             InitializeComponent();
         }
--- laba1/Program.cs
+++ laba1/Program.cs
@@ -16,7 +16,7 @@ namespace laba1
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new laba1());
+            Application.Run(new Form1());
         }
     }
 }
